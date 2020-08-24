d293f53c7c191a9f6308a3fbcef364b351b13921
Corbin Albert <corbin.albert@gmail.com>
2020-08-23 23:50:39 -0700

    Write first happy-path test for is_open

--- tests/test_check_webpage.py
+++ tests/test_check_webpage.py
@@ -2,5 +2,9 @@ from irish_fbr.check_website import is_open
 
 
 def test_is_open():
-    fail_response = "this string does not include the correct term"
-    assert not is_open(fail_response)
+    assert not is_open(
+        "Please note that we have stopped processing Foreign Birth Registrations"
+    )
+
+    fail_response = "We're open for business!"
+    assert is_open(fail_response)
