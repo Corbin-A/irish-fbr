1a3b7435c9d22ffeb68854956963cb9c779b2cea
Corbin Albert <corbin.albert@gmail.com>
2020-08-23 23:10:25 -0700

    change name from irish-fbr to irish_fbr

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# irish-fbr
+# irish_fbr
 Tiny project to help me determine when Irish Foreign Birth Registry opens (currently closed due to COVID)
---
+++ irish_fbr/check_website.py
@@ -0,0 +1,43 @@
+import requests
+from twilio.rest import Client
+from dotenv import load_dotenv, find_dotenv
+import os
+
+load_dotenv(find_dotenv())
+
+
+def spin_up_twilio() -> Client:
+    account_sid = os.getenv("TWILIO_SID")
+    auth_token = os.getenv("TWILIO_TOKEN")
+    return Client(account_sid, auth_token)
+
+
+def text_alex(msg: str) -> None:
+    client = spin_up_twilio()
+    client.messages.create(
+        body=msg, from_=os.getenv("TWILIO_FROM"), to=os.getenv("TWILIO_TO"),
+    )
+
+
+def is_open(response: requests.models.Response) -> bool:
+    return (
+        "Please note that we have stopped processing Foreign Birth Registrations"
+        not in response.text
+    )
+
+
+def fetch_webpage(url: str) -> str:
+    return requests.get(url)
+
+
+def fetch_irish_fbr_webpage() -> str:
+    return fetch_webpage(
+        "https://www.dfa.ie/citizenship/born-abroad/registering-a-foreign-birth/"
+    )
+
+
+if __name__ == "__main__":
+    response = fetch_irish_fbr_webpage()
+
+    if is_open(response):
+        text_alex("Irish Foreign Birth Register website has changed.")
